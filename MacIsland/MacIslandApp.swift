a96df509cc96242258c47f6c4547abfc21dc8ca4
Joshua Wong <joshuawong@Joshuas-Laptop.local>
2025-08-31 14:28:54 -0700

    first commit

---
+++ MacIsland/MacIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacIslandApp.swift
+//  MacIsland
+//
+//  Created by Joshua Wong on 2025-08-30.
+//
+
+import SwiftUI
+
+@main
+struct MacIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacIslandTests/MacIslandTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacIslandTests.swift
+//  MacIslandTests
+//
+//  Created by Joshua Wong on 2025-08-30.
+//
+
+import Testing
+@testable import MacIsland
+
+struct MacIslandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
